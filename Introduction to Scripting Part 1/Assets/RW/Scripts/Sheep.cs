cdd17ddf07ad4ffa0933451478cd07b1ecbe871f
croyn <batchild2004@hotmail.com>
2020-04-23 18:07:13 +0200

    subida

---
+++ Introduction to Scripting Part 1/Assets/RW/Scripts/Sheep.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sheep : MonoBehaviour
+{
+
+    public float runSpeed;
+    public float gotHayDestroyDelay;
+    private bool hitByHay;
+    public float dropDestroyDelay; // 1
+    private Collider myCollider; // 2
+    private Rigidbody myRigidbody;
+    private SheepSpawner sheepSpawner;
+    public float heartOffset; // 1
+    public GameObject heartPrefab; // 2
+    // Start is called before the first frame update
+    void Start()
+    {
+        myCollider = GetComponent<Collider>();
+        myRigidbody = GetComponent<Rigidbody>();
+        
+    }
+
+    public void SetSpawner(SheepSpawner spawner)
+    {
+        sheepSpawner = spawner;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * runSpeed * Time.deltaTime);
+    }
+    private void HitByHay()
+    {
+        SoundManager.Instance.PlaySheepHitClip();
+        sheepSpawner.RemoveSheepFromList(gameObject);
+        hitByHay = true; // 1
+        runSpeed = 0; // 2
+        Destroy(gameObject, gotHayDestroyDelay); // 3
+        Instantiate(heartPrefab, transform.position + new Vector3(0, heartOffset, 0), Quaternion.identity);
+        TweenScale tweenScale = gameObject.AddComponent<TweenScale>(); ; // 1
+        tweenScale.targetScale = 0; // 2
+        tweenScale.timeToReachTarget = gotHayDestroyDelay; // 3
+        GameStateManager.Instance.SavedSheep();
+
+    }
+
+    
+
+    private void Drop()
+    {
+        SoundManager.Instance.PlaySheepDroppedClip();
+        sheepSpawner.RemoveSheepFromList(gameObject);
+        GameStateManager.Instance.DroppedSheep();
+        myRigidbody.isKinematic = false; // 1
+        myCollider.isTrigger = false; // 2
+        Destroy(gameObject, dropDestroyDelay); // 3
+    }
+
+    private void OnTriggerEnter(Collider other) // 1
+    {
+        if (other.CompareTag("Hay") && !hitByHay) // 2
+        {
+            Destroy(other.gameObject); // 3
+            HitByHay(); // 4
+        }
+        else if (other.CompareTag("DropSheep"))
+        {
+            Drop();
+        }
+
+    }
+
+
+
+}
